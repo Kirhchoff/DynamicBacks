f8a013d11316cce8761c68cd27059ae1d70575ae
Krzysztof Kirsz <k.kirsz@gmail.com>
2016-12-29 10:40:39 +0100

    Additional style parameter, inhibit new animation trigger while the last one is still in progress

--- DynamicBacks.js
+++ DynamicBacks.js
@@ -3,18 +3,20 @@ function createDynamicBack() {
   var _targetSel = "";
   var _dynamicBackBaseTmpl="._dynamic_back:before,._dynamic_back:after{content:\"\";position:absolute;top:0px;right:0px;bottom:0px;left:0px;opacity:1;background-color:{0}}";
   var _dynamicBackTmpl="._dynamic_back{0}:before{background-image:url({1});background-size:cover;z-index:-1;}\
-  ._dynamic_back{0}:after{background-image:url({2});background-size:cover;z-index:-2;}";
+  ._dynamic_back{0}:after{background-image:url({2});background-size:cover;z-index:-2;{3}}";
   var _faderTmpl="._dynamic_back_fader:before{opacity:0;transition:opacity {0}s;}";
-  var _current_back = 0;
+  var _currentBack = 0;
+  var _inProgress = false;
 
   function _setTransitionEndHandler() {    
     $(_targetSel).on("transitionend webkitTransitionEnd", function(e){
       var numOfBacks = _backs.length;
-      currentIndex = (_current_back-1) % numOfBacks;
-      nextIndex = _current_back % numOfBacks;
+      currentIndex = (_currentBack-1) % numOfBacks;
+      nextIndex = _currentBack % numOfBacks;
       $(this).addClass("_dynamic_back"+nextIndex);
       $(this).removeClass("_dynamic_back_fader");
       $(this).removeClass("_dynamic_back"+currentIndex);
+      _inProgress = false;
     });    
   };
   
@@ -28,11 +30,11 @@ function createDynamicBack() {
     });
   }
 
-  function _createDynamicBackCSS(duration, color){
+  function _createDynamicBackCSS(duration, color, additionalStyle){
     var numOfBacks = _backs.length;
     $("<style type='text/css'>"+_formatString(_dynamicBackBaseTmpl, color)+"</style>").appendTo("head");
     for(var i = 0; i<numOfBacks; i++){
-      $("<style type='text/css'>"+_formatString(_dynamicBackTmpl,i,_backs[i], _backs[(i+1)%numOfBacks])+"</style>").appendTo("head");
+      $("<style type='text/css'>"+_formatString(_dynamicBackTmpl,i,_backs[i], _backs[(i+1)%numOfBacks],additionalStyle)+"</style>").appendTo("head");
     }
     $("<style type='text/css'>"+_formatString(_faderTmpl, duration)+"</style>").appendTo("head");
   };
@@ -54,19 +56,22 @@ function createDynamicBack() {
     return true;
   }
 
-  function initialize(backs, targetSel, duration=0.6, color="white"){
+  function initialize(backs, targetSel, duration=0.6, color="white", additionalStyle=""){
     if(_validateInput(backs, targetSel)){
       _backs = backs;
       _targetSel = targetSel;
-      _createDynamicBackCSS(duration, color);
+      _createDynamicBackCSS(duration, color, additionalStyle);
       _setInitialBack();
       _setTransitionEndHandler();
     }
   };
   
   function nextBack(){
-    _current_back++;
-    $(_targetSel).addClass("_dynamic_back_fader");
+    if(!_inProgress){
+      _currentBack++;
+      _inProgress = true;
+      $(_targetSel).addClass("_dynamic_back_fader");
+    }
   };
   
   return {
--- README.md
+++ README.md
@@ -4,16 +4,22 @@ Javascript library for handling dynamic background-images.
 ## Usage
 1. Create instance of dynamic backgrounds handler for the object you want to animate:
   var dynaBack = createDynamicBack();
-2. Initialize object with an array of background images, target object jQuery selector, duration of transition animation in seconds, background color:
-  dynaBack.initialize(backgrounds, "#target", 0.4, "magenta");
+2. Initialize object with an array of background images, target object jQuery selector, duration of transition animation in seconds, background color, additional CSS style to append, using _initialize_ method:
+  initialize(backgroundsArray, targetJQuerySelector, [duration, backgroundColor, additionalStyle]),
   
-Default values:
+  for example:
+  dynaBack.initialize(backgrounds, "#target", 0.4, "magenta", "background-position:center");
+  
+Only the first two parameters are required. The rest has default values:
   - animation duration: 0.6,
   - background color: "white"
+  - additional style: ""
   
 IMPORTANT: target element cannot have it's own background color specified in css. This will cover the animations. You can only specify the background color via aforementioned interface.
 3. Trigger animation:
   dynaBack.nextBack();
+  
+  During the animation, trigger is disabled, so you cannot start another animation until the previous one finished.
 
 ## Example
 https://codepen.io/Kirhchoff/full/aBgyae/
